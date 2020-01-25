f7c5c4e2eeac099e5ed3ddc36a67f40ca7a5cf9a
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-25 16:16:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event_name){
+    let answer = []
+    for (let i = 0; i < names.length; i++){
+        answer.push( `Thank you, ${names[i]}, for the wonderful ${event_name} gift!`)
+    }
+    return answer
+}
+
+function countDown(number){ 
+    while ( number > 0 ){ 
+        console.log(number); 
+        number -= 1; 
+    }
+    console.log(number)
+}
