43c78566646c926c74be8c80f4fec65cbcd37a30
Amia Defreitas <amia.defreitas0@gmail.com>
2020-01-26 18:40:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards( names, reason ) {
+    let thankyoucards = []
+    for ( let i = 0; i < names.length; i++ ) {
+      thankyoucards.push( `Thank you, ${names[i]}, for the wonderful ${reason} gift!` )
+    }
+    return thankyoucards
+  }
+
+  function countDown(number){
+
+      while(number > 0){
+          console.log(number);
+          number -= 1;
+      }
+      console.log(number);
+  }
